7970588dc8a1e1866214215e2f045e6b1d21d8f9
Mayank <mayank4116@gmail.com>
2019-05-30 00:39:03 +0530

    added admin home page

---
+++ frontend/src/components/Admin/AdminHome/AdminHome.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import {Link} from 'react-router-dom';
+
+const AdminHome=()=>{
+    return(
+        <div className="adminHomeBackground white">
+            <h2 className="ma0">Welcome Admin</h2><br></br><br/>
+            <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+                <Link to="/addstudent" className="noUnderline">
+                    <Button variant="contained" color="secondary">
+                        Add Student
+                    </Button>
+                </Link>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Link to="/removestudent" className="noUnderline">
+                <Button variant="contained" color="secondary">
+                    Remove Student
+                </Button>
+                </Link>
+            </Grid>
+            <Grid item xs={12}>
+                <hr width="80%" align="center"></hr>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Link to="/addfaculty" className="noUnderline">
+                <Button variant="contained" color="secondary">
+                    Add Faculty
+                </Button></Link>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Link to="/removefaculty" className="noUnderline">
+                    <Button variant="contained" color="secondary">
+                        Remove Faculty
+                    </Button>
+                </Link>
+            </Grid>
+            <Grid item xs={12}>
+                <hr width="80%" align="center"></hr>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Link to="/addclass" className="noUnderline">
+                <Button variant="contained" color="secondary">
+                    Add Class
+                </Button>
+                </Link>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Link to="/removeclass" className="noUnderline">
+                    <Button variant="contained" color="secondary">
+                        Remove Class
+                    </Button>
+                </Link>
+            </Grid>
+            <Grid item xs={12}>
+                <hr width="80%" align="center"></hr>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            <Link to="/addcourse" className="noUnderline">
+            <Button variant="contained" color="secondary">
+                Add Course
+                </Button></Link>
+            </Grid>
+                <Grid item xs={12} sm={6}>
+            <Link to="/removecourse" className="noUnderline">
+            <Button variant="contained" color="secondary">
+                Remove Course
+                </Button></Link>
+                </Grid>
+                <Grid item xs={12}>
+                    <hr width="80%" align="center"></hr>
+                </Grid>
+            <Grid item xs={12} sm={6}>
+            <Link to="/assigncourse" className="noUnderline">
+            <Button variant="contained" color="secondary">
+                Assign Course to Student
+                </Button></Link>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            <Link to="/deassigncourse" className="noUnderline">
+            <Button variant="contained" color="secondary">
+                Deassign Course to Student
+                </Button></Link>
+            </Grid>
+            <Grid item xs={12}>
+                <hr width="80%" align="center"></hr></Grid>
+                <Grid item xs={12} sm={6}>
+            <Link to="/assignfaculty" className="noUnderline">
+            <Button variant="contained" color="secondary">
+                Assign Class to Faculty
+                </Button></Link>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+            <Link to="/deassignfaculty" className="noUnderline">
+            <Button variant="contained" color="secondary">
+                Deassign Class to Faculty
+                </Button></Link></Grid>
+            </Grid>
+        </div>
+    )
+};
+
+export default AdminHome;
--- frontend/src/components/Routes/Routes.js
+++ frontend/src/components/Routes/Routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom'
 
 import Home from '../Home/Home';
+import AdminHome from '../Admin/AdminHome/AdminHome';
 
 const Routes=()=>{
     return(
         <Switch>
+            <Route path="/admin" exact component={AdminHome}></Route>
             <Route path="/" exact component={Home}></Route>
         </Switch>
     );
